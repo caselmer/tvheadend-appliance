26e3ce9a9c3665bdf302d7d2f4c78985d77c2f56
Christian Selmer <ablage_p@yahoo.de>
2026-07-19 20:32:52 +0200

    add TVHeadend repository configuration

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -61,12 +61,47 @@ install_dependencies() {
 
     install_package curl
     install_package ca-certificates
-    install_package wget
     install_package jq
+    install_package gnupg
+    install_package debian-keyring
+    install_package debian-archive-keyring
+    # install_package etckeeper
 
     success "Abhängigkeiten installiert."
 
 }
+
+
+configure_tvheadend_repository() {
+
+    info "Richte TVHeadend-Repository ein..."
+
+    local keyring="/usr/share/keyrings/tvheadend-tvheadend-archive-keyring.gpg"
+    local repo="/etc/apt/sources.list.d/tvheadend-tvheadend.list"
+
+    if [[ -f "$repo" ]]; then
+        success "TVHeadend-Repository bereits eingerichtet."
+        return
+    fi
+
+    curl -1sLf \
+        "https://dl.cloudsmith.io/public/tvheadend/tvheadend/gpg.C6CC06BD69B430C6.key" \
+        | gpg --dearmor -o "$keyring"
+
+    curl -1sLf \
+        "https://dl.cloudsmith.io/public/tvheadend/tvheadend/config.deb.txt?distro=debian&codename=bookworm&component=main" \
+        -o "$repo"
+
+    chmod 644 "$keyring"
+    chmod 644 "$repo"
+
+    apt-get update
+
+    success "TVHeadend-Repository eingerichtet."
+
+}
+
+
 ###############################################################################
 # Cleanup
 ###############################################################################
@@ -90,5 +125,5 @@ install_system() {
     update_package_lists
     upgrade_system
     install_dependencies
-
+    configure_tvheadend_repository
 }
