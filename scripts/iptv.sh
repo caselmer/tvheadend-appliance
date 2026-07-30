012df2defe9b02f1b126dd401bc63eb65eaed60c
Christian Selmer <ablage_p@yahoo.de>
2026-07-30 18:43:12 +0200

    Complete TVHeadend API initialization workflow

--- scripts/iptv.sh
+++ scripts/iptv.sh
@@ -16,3 +16,48 @@ configure_iptv() {
     trigger_scan
 
 }
+
+###############################################################################
+# IPTV network
+###############################################################################
+
+create_iptv_network() {
+
+    info "IPTV-Netzwerk wird vorbereitet..."
+
+    # TODO:
+    # IPTV-Netzwerk über TVHeadend API anlegen.
+
+    success "IPTV-Netzwerk vorbereitet."
+
+}
+
+###############################################################################
+# M3U
+###############################################################################
+
+configure_m3u() {
+
+    info "M3U-Konfiguration wird vorbereitet..."
+
+    # TODO:
+    # M3U-Quelle konfigurieren.
+
+    success "M3U-Konfiguration übersprungen."
+
+}
+
+###############################################################################
+# Scan
+###############################################################################
+
+trigger_scan() {
+
+    info "Sendersuchlauf wird vorbereitet..."
+
+    # TODO:
+    # Scan starten.
+
+    success "Sendersuchlauf übersprungen."
+
+}
